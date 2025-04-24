5a6613de70bb6fa0de6fe5114a3074109aaa9677
hemantkumar2323 <freelanceb23@gmail.com>
2025-04-24 12:51:42 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AIToolsFinderAgent from './AIToolsFinderAgent.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AIToolsFinderAgent />
+  </React.StrictMode>
+)
